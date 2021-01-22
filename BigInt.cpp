d271a7c1e0a8ab1465454ebde70e401390c76af2
Axel Ouari <ouari-axel@hotmail.com>
2021-01-22 22:25:25 +0100

    BigInt

--- BigInt.cpp
+++ BigInt.cpp
@@ -87,13 +87,13 @@ unsigned int Addition(vector<unsigned int>& result, vector<unsigned int>& A, vec
 				break;
 		}
 	}
-	
+
 	return carry;
 
 }
 
 //multiplication des paquets
-unsigned int Multiplication(vector<unsigned int>& result, vector<unsigned int>& A, vector<unsigned int>& B, vector<unsigned int>& carry, unsigned int taille)
+void Multiplication(vector<unsigned int>& result, vector<unsigned int>& A, vector<unsigned int>& B, vector<unsigned int>& carry, unsigned int taille)
 {
 	unsigned int k = 0;
 	vector<unsigned int> temp;
@@ -106,7 +106,7 @@ unsigned int Multiplication(vector<unsigned int>& result, vector<unsigned int>& 
 		}
 
 	decalageBit(temp, A, k);
-	
+
 	for(unsigned int i = 0; i < temp.size(); i++)
 		result.push_back(temp[i]);
 
@@ -120,15 +120,15 @@ unsigned int Multiplication(vector<unsigned int>& result, vector<unsigned int>& 
 		if(B[i] == 1)
 		{
 			k = 0;
-			
+
 			decalageBit(temp, A, i);
 
 			k = Addition(temp2, result, temp, k);
 			if(k == 1)
 				temp2.push_back(k);
-			
+
 			result.clear();
-			
+
 			for(unsigned int i = 0; i < temp2.size(); i++)
 				result.push_back(temp2[i]);
 
@@ -136,24 +136,22 @@ unsigned int Multiplication(vector<unsigned int>& result, vector<unsigned int>& 
 
 			temp.clear();
 		}
-		
+
 	k = taille;
-	
+
 	if(result.size() > k)
 	{
 		for(unsigned int i = k; i < result.size(); i++)
 			carry.push_back(result[i]);
-			
+
 		l = result.size();
-		
+
 		for(unsigned int i = k; i < l; i++)
 			result.pop_back();
 	}
-	
-	return k;
 }
 
-//addition grand nombre 
+//addition grand nombre
 void addition(vector<vector<unsigned int> >& result, vector<vector<unsigned int> >& A, vector<vector<unsigned int> >& B)
 {
 	unsigned int carry = 0;
@@ -186,55 +184,55 @@ void soustraction(vector<vector<unsigned int> >& result, vector<vector<unsigned 
 	if(A.size() > B.size())
 	{
 		unsigned int taille = A.size();
-		
+
 		vector<vector<unsigned int> > tempB;
 		vector<unsigned int> tempPaquet;
-		
+
 		for(unsigned int i = 0; i < B.size(); i++)
 			tempB.push_back(tempPaquet);
-		
+
 		for(unsigned int i = 0; i < B.size(); i++)
 			for(unsigned int j = 0; j < B[i].size(); j++)
 				tempB[i].push_back(1 - B[i][j]);
-				
+
 		tempPaquet.push_back(1);
-		
+
 		vector<vector<unsigned int> > temp;
 		temp.push_back(tempPaquet);
-				
+
 		vector<vector<unsigned int> > tempB2;
 		addition(tempB2, tempB, temp);
-		
+
 		addition(result, A, tempB2);
-		
+
 		if(result.size() != taille)
 			result.pop_back();
 	}
-	
-	else 
+
+	else
 	{
 		unsigned int taille = B.size();
-	
+
 		vector<vector<unsigned int> > tempA;
 		vector<unsigned int> tempPaquet;
-		
+
 		for(unsigned int i = 0; i < A.size(); i++)
 			tempA.push_back(tempPaquet);
-		
+
 		for(unsigned int i = 0; i < A.size(); i++)
 			for(unsigned int j = 0; j < A[i].size(); j++)
 				tempA[i].push_back(1 - A[i][j]);
-				
+
 		tempPaquet.push_back(1);
-		
+
 		vector<vector<unsigned int> > temp;
 		temp.push_back(tempPaquet);
-				
+
 		vector<vector<unsigned int> > tempA2;
 		addition(tempA2, tempA, temp);
-		
+
 		addition(result, B, tempA2);
-		
+
 		if(result.size() != taille)
 			result.pop_back();
 	}
@@ -250,73 +248,71 @@ void multiplication(vector<vector<unsigned int> >& result, vector<vector<unsigne
 	unsigned int l;
 	unsigned int j = taille;
 	vector<unsigned int> carry;
-	
-	
+
+
 	vector<unsigned int> tempResultat;
-	
-	j = Multiplication(tempResultat, A[0], B[0], carry, j);
-	
+
+	Multiplication(tempResultat, A[0], B[0], carry, j);
+
 	result.push_back(tempResultat);
-	
+
 	tempResultat.clear();
-	
+
 	vector<unsigned int> tempProduit;
 	tempResultat.push_back(0);
-	
+
 	for (unsigned int k = 1; k < A.size() + B.size() - 1; k++)
-	{	
+	{
 		for (unsigned int i = 0; i <= k ; i++)
-		{	
+		{
 			if ((i < A.size()) && (k - i < B.size()))
 			{
 				tempProduit.clear();
-				
-				unsigned int m = 2 * j;
-				
-				m = Multiplication(tempProduit, A[i], B[k - i], carry, m);
-				
+
+				Multiplication(tempProduit, A[i], B[k - i], carry, 2 * j);
+
 				vector<unsigned int> tempPaquet;
-				
+
 				l = 0;
 				l = Addition(tempPaquet, tempResultat, tempProduit, l);
 				if(l == 1)
 					tempPaquet.push_back(l);
-				
+
 				tempResultat.clear();
-				
+
 				for(unsigned int i = 0; i < tempPaquet.size(); i++)
 					tempResultat.push_back(tempPaquet[i]);
-					
+
 				tempPaquet.clear();
 			}
 		}
-		
+
 		vector<unsigned int> tempResultat2;
 		l = 0;
 		l = Addition(tempResultat2, tempResultat, carry, l);
 		if(l == 1)
 			tempResultat2.push_back(l);
-			
+
 		tempResultat.clear();
 		for(unsigned int  i = 0; i < tempResultat2.size(); i++)
 			tempResultat.push_back(tempResultat2[i]);
-			
+
 		carry.clear();
-		
+
 		if(tempResultat.size() > j)
 		{
 			for(unsigned int i = j; i < tempResultat.size(); i++)
 				carry.push_back(tempResultat[i]);
-				
+
 			unsigned int n = tempResultat.size();
-			
+
 			for (unsigned int i = j; i < n; i++)
 				tempResultat.pop_back();
 		}
-		
+
 		result.push_back(tempResultat);
 	}
-	
+
 	result.push_back(carry);
 }
 
@@ -325,20 +321,20 @@ int main()
 {
 	vector<unsigned int> package;
 	vector<unsigned int> package2;
-	
+
 	numberConverter(package, 5, 4);
 	numberConverter(package2, 10, 4);
 
 	vector<vector<unsigned int> > number;
 	vector<vector<unsigned int> > number2;
-	
+
 
 	number.push_back(package);
 	number.push_back(package);
 	number2.push_back(package2);
 	number2.push_back(package2);
-	
-	
+
+
 	cout << "premier nombre (85) : ";
 	for (int i = 0; i < number.size(); i++){
 		for (int j = 0; j < number[i].size(); j++)
@@ -348,7 +344,7 @@ int main()
 		cout << " | ";
 	}
 	cout << endl << endl;
-	
+
 	cout << "deuxième nombre (170) : ";
 	for (int i = 0; i < number2.size(); i++){
 		for (int j = 0; j < number2[i].size(); j++)
@@ -358,9 +354,9 @@ int main()
 		cout << " | ";
 	}
 	cout << endl << endl;
-	
+
 	vector<vector<unsigned int> > result;
-	
+
 	cout << "somme (185 + 75) : ";
 	addition(result, number, number2);	//Somme 85 + 170 (01010101 + 10101010)
 	for (int i = 0; i < result.size(); i++){
@@ -395,10 +391,10 @@ int main()
 
 	vector<vector<unsigned int> > number3;
 	vector<vector<unsigned int> > number4;
-	
+
 	for (unsigned int i = 1; i <= 32; i++)
         number3.push_back(package3);
-		
+
 	for (unsigned int i = 1; i <= 32; i++)
         number4.push_back(package4);
 
@@ -411,7 +407,7 @@ int main()
 		cout << " | ";
 	}
 	cout << endl << endl;
-	
+
 	cout << "deuxième nombre : " << endl;
 	for (int i = 0; i < number4.size(); i++){
 		for (int j = 0; j < number4[i].size(); j++)
@@ -421,9 +417,9 @@ int main()
 		cout << " | ";
 	}
 	cout << endl << endl;
-	
+
 	vector<vector<unsigned int> > result2;
-	
+
 	cout << "addition : " << endl;
 	addition(result2, number3, number4);	//le résultat est un BigInt avec que des 1
 	for (int i = 0; i < result2.size(); i++){
@@ -434,7 +430,7 @@ int main()
 		cout << " | ";
 	}
 	cout << endl << endl;
-	
+
 	vector<unsigned int> packagebis;
 
 	numberConverter(packagebis, 4294967295, 32);
@@ -467,7 +463,7 @@ int main()
 		cout << " | ";
 	}
 	cout << endl << endl;
-	
+
 	cout << "soustraction : " << endl; //on obtient bien 0 version BigInt
 	soustraction(resultbis3, numberbis, numberbis);
 	for (int i = 0; i < resultbis3.size(); i++){
@@ -478,7 +474,7 @@ int main()
 		cout << " | ";
 	}
 	cout << endl << endl;
-	
+
 	cout << "multiplication : " << endl; //problème au sein de la multiplication au bout du 34ème paquet
 	multiplication(resultbis2, numberbis, numberbis, 32);
 	for (int i = 0; i < resultbis2.size(); i++){
@@ -489,7 +485,7 @@ int main()
 		cout << " | ";
 	}
 	cout << endl << endl;
-	
+
 	return 0;
 }
 
